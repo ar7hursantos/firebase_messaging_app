06b9718bd0d55ebc46c29865327dd834acc36052
webpacs <webpacs.co@gmail.com>
2020-10-19 22:49:50 -0300

    remocao comentario

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,6 @@ import 'app/app_module.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO! Se a inicializacao do FirebaseMessaging for iniciada na linha abaixo, ao invés da linha 17 da classe AppPushsPage,
-  // TODO! a mensagem não será lida corretamente quando for selecionada e o app estiver completamente fechado.
-  //await NotificationHandler().initializeFcmNotification();
-
   runApp(
     ModularApp(module: AppModule()),
   );
